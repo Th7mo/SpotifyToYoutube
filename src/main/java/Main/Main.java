8ba705a146d74ef7a22914776628fe8057dc25dd
Thimo van Velzen <thimoquinten@gmail.com>
2021-11-08 16:42:20 +0100

    Add Basic Logging in Main to demonstrate that a Access Token is retrieved from Spotify API

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,5 +1,8 @@
 package Main;
 
+import DAO.SpotifyTokenDAO;
+import Model.SpotifyToken;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,6 +10,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Logger.class.getName());
-		logger.log(Level.INFO, "Hello!");
+		logger.log(Level.INFO, "Trying to get Spotify Access Token...");
+		SpotifyTokenDAO spotifyTokenDAO = new SpotifyTokenDAO();
+
+		try {
+			SpotifyToken token = spotifyTokenDAO.getToken();
+			logger.log(Level.INFO, "Attempt successful. \n" +
+					"Access token: " + token.getAccess_token());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
