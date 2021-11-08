5b42ff0f47294b10d9587795ef1e85d6ca2579d1
th7mo <dev@th7mo.com>
2021-11-08 16:41:39 +0100

    Add Unit tests to test functionality of SpotifyTokenDAO

--- src/test/java/DAO/SpotifyTokenDAOTests.java
+++ src/test/java/DAO/SpotifyTokenDAOTests.java
@@ -1,2 +1,64 @@
-package DAO;public class SpotifyTokenDAOTests {
+package DAO;
+
+import Model.SpotifyToken;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+public class SpotifyTokenDAOTests {
+
+	private SpotifyToken spotifyToken;
+
+	@BeforeEach
+	public void setUp() throws IOException {
+		SpotifyTokenDAO spotifyTokenDAO = new SpotifyTokenDAO();
+		spotifyToken = spotifyTokenDAO.getToken();
+	}
+
+	@Test
+	public void Should_SayAccessTokenIsNotNullWhenGettingToken() {
+		String access_token = spotifyToken.getAccess_token();
+
+		assertNotNull(access_token);
+	}
+
+	@Test
+	public void Should_SayTokenTypeIsNotNullWhenGettingToken() {
+		String token_type = spotifyToken.getToken_type();
+
+		assertNotNull(token_type);
+	}
+
+	@Test
+	public void Should_SayExpiresInIs3600WhenGettingToken() {
+		int expected_expires_in = 3600;
+		int expires_in = spotifyToken.getExpires_in();
+
+		assertEquals(expires_in, expected_expires_in);
+	}
+
+	@Test
+	public void Should_SayAccessTokenLengthIsEightyThreeWhenGettingToken() {
+		int expectedTokenLength = 83;
+		String access_token = spotifyToken.getAccess_token();
+		int tokenLength = access_token.length();
+
+		assertEquals(tokenLength, expectedTokenLength);
+	}
+
+	@Test
+	public void Should_SayTokenTypeIsBearerWhenGettingToken() {
+		String expectedTokenType = "Bearer";
+		String token_type = spotifyToken.getToken_type();
+
+		assertEquals(token_type, expectedTokenType);
+	}
+
+	@Test
+	public void Should_NotCrashWhenCredentialsAreInvalid() {
+
+	}
 }
