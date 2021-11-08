08de0b39473b495427f2e85c4fdcea0880a79c94
Thimo van Velzen <thimoquinten@gmail.com>
2021-11-08 14:10:21 +0100

    Initial commit

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Logger.class.getName());
+		logger.log(Level.INFO, "Hello!");
+	}
+}
