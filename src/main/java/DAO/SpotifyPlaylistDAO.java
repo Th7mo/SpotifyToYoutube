0d09b15a5a4f1ca51ab8b8550d3feecd38d01ad1
Thimo van Velzen <thimoquinten@gmail.com>
2021-11-09 10:17:09 +0100

    Make that public playlist can be retrieved from Spotify API as jsonString

--- src/main/java/DAO/SpotifyPlaylistDAO.java
+++ src/main/java/DAO/SpotifyPlaylistDAO.java
@@ -1,16 +1,63 @@
 package DAO;
 
+import Model.AuthenticationOptions;
 import Model.Playlist;
 import Model.SpotifyPlaylist;
+import Model.SpotifyToken;
+import com.google.gson.Gson;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class SpotifyPlaylistDAO implements PlaylistDAO {
+
+	private HttpURLConnection connection;
+	private String accessToken;
+	private final String PLAYLIST_URL = "https://api.spotify.com/v1/playlists/0rBbrxmYoRfekosc63tbss/tracks";
 
 	public SpotifyPlaylistDAO() {}
 
 	@Override
-	public Playlist getPlaylist() throws IOException {
-		return new SpotifyPlaylist();
+	public Playlist getPlaylist(String accessToken) throws IOException {
+		this.accessToken = accessToken;
+		initializeConnection();
+		String responseJson = getResponse();
+		System.out.println(responseJson);
+
+		return getBuildPlaylist(responseJson);
 	}
+
+	private void initializeConnection() throws IOException {
+		URL url = new URL(PLAYLIST_URL);
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Authorization",
+				"Bearer " + accessToken);
+		connection.setRequestProperty("Content-Type", "application/json");
+	}
+
+	private String getResponse() throws IOException {
+		BufferedReader reader = getBufferedReader();
+
+		return buildResponseString(reader);
+	}
+
+	private BufferedReader getBufferedReader() throws IOException {
+		InputStream inputStream = connection.getInputStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+		return new BufferedReader(inputStreamReader);
+	}
+
+	private String buildResponseString(BufferedReader reader) {
+		return reader.lines().collect(Collectors.joining());
+	}
+
+	private Playlist getBuildPlaylist(String json) {
+		return new Gson().fromJson(json, SpotifyPlaylist.class);
+	}
+
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,10 @@
 package Main;
 
+import DAO.PlaylistDAO;
+import DAO.SpotifyPlaylistDAO;
 import DAO.SpotifyTokenDAO;
 import DAO.TokenDAO;
+import Model.SpotifyPlaylist;
 import Model.Token;
 
 import java.util.logging.Level;
@@ -13,11 +16,13 @@ public class Main {
 		Logger logger = Logger.getLogger(Logger.class.getName());
 		logger.log(Level.INFO, "Trying to get Spotify Access Token...");
 		TokenDAO spotifyTokenDAO = new SpotifyTokenDAO();
+		PlaylistDAO playlistDAO = new SpotifyPlaylistDAO();
 
 		try {
 			Token token = spotifyTokenDAO.getToken();
 			logger.log(Level.INFO, "Attempt successful. \n" +
 					"Access token: " + token.getAccess_token());
+			playlistDAO.getPlaylist(token.getAccess_token());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
