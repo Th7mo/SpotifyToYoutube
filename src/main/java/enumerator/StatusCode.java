1d5ca2fd7925616ebc7e36a273085eea50aeafcf
Thimo van Velzen <thimoquinten@gmail.com>
2021-11-14 15:36:21 +0100

    Rename STATUS_CODE to StatusCode since that is the correct way to name enums

--- src/main/java/enumerator/StatusCode.java
+++ src/main/java/enumerator/StatusCode.java
@@ -1,13 +1,13 @@
 package enumerator;
 
-public enum STATUS_CODE {
+public enum StatusCode {
 	OK (200),
 	BAD_REQUEST (400),
 	UNAUTHORIZED (401);
 
 	private final int codeNumber;
 
-	STATUS_CODE(int codeNumber) {
+	StatusCode(int codeNumber) {
 		this.codeNumber = codeNumber;
 	}
 
