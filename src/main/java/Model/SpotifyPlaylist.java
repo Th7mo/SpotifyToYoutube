381aad30c9edf63957471f807f6f9cd5ec584335
Thimo van Velzen <thimoquinten@gmail.com>
2021-11-14 14:09:42 +0100

    Add that Youtube playlist gets actually updated

--- src/main/java/Model/SpotifyPlaylist.java
+++ src/main/java/Model/SpotifyPlaylist.java
@@ -3,44 +3,37 @@ package Model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpotifyPlaylist implements Playlist {
+public class SpotifyPlaylist {
 
 	private List<Item> items = new ArrayList<>();
 	private String name;
 
 	public SpotifyPlaylist() {}
 
-	@Override
 	public List<Item> getItems() {
 		return items;
 	}
 
-	@Override
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
 
-	@Override
-	public void join(Playlist playlist) {
+	public void join(SpotifyPlaylist playlist) {
 		items.addAll(playlist.getItems());
 	}
 
-	@Override
 	public boolean isEmpty() {
 		return items.isEmpty();
 	}
 
-	@Override
 	public int size() {
 		return items.size();
 	}
 
-	@Override
 	public String getTitle() {
 		return name;
 	}
 
-	@Override
 	public void setTitle(String title) {
 		this.name = title;
 	}
