6bb57b66fa19090fdb326938e2bd834aa063a581
th7mo <dev@th7mo.com>
2021-11-15 15:01:37 +0100

    Refactor insertPlaylistItem method in YoutubePlaylistItemDAO so that functionalities are split into seperate methods

--- src/main/java/dao/YoutubePlaylistItemDAO.java
+++ src/main/java/dao/YoutubePlaylistItemDAO.java
@@ -13,20 +13,30 @@ public class YoutubePlaylistItemDAO {
 	private YouTube youtube;
 
 	public void insertPlaylistItem(String playlistId, String videoId) throws IOException {
+		PlaylistItemSnippet playlistItemSnippet = getPlaylistItemSnippet(playlistId, videoId);
+		PlaylistItem playlistItem = new PlaylistItem();
+		playlistItem.setSnippet(playlistItemSnippet);
+		YouTube.PlaylistItems.Insert playlistItemsInsertCommand = youtube.playlistItems()
+				.insert(Collections.singletonList("snippet,contentDetails"), playlistItem);
+		playlistItemsInsertCommand.execute();
+	}
+
+	private ResourceId getResourceId(String videoId) {
 		ResourceId resourceId = new ResourceId();
 		resourceId.setKind("youtube#video");
 		resourceId.setVideoId(videoId);
 
+		return resourceId;
+	}
+
+	private PlaylistItemSnippet getPlaylistItemSnippet(String playlistId,
+													   String videoId) {
+		ResourceId resourceId = getResourceId(videoId);
 		PlaylistItemSnippet playlistItemSnippet = new PlaylistItemSnippet();
 		playlistItemSnippet.setPlaylistId(playlistId);
 		playlistItemSnippet.setResourceId(resourceId);
 
-		PlaylistItem playlistItem = new PlaylistItem();
-		playlistItem.setSnippet(playlistItemSnippet);
-
-		YouTube.PlaylistItems.Insert playlistItemsInsertCommand = youtube.playlistItems()
-				.insert(Collections.singletonList("snippet,contentDetails"), playlistItem);
-		playlistItemsInsertCommand.execute();
+		return playlistItemSnippet;
 	}
 
 	public void setYoutube(YouTube youtube) {
