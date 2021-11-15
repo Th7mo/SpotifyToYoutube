1b94b85223e37a8e6de2a8cab6620504416d4a7c
th7mo <dev@th7mo.com>
2021-11-15 17:02:36 +0100

    Add SpotifyPlaylistDAO class that will get the title of the Spotify Playlist

--- src/test/java/dao/SpotifyPlaylistTitleDAOTests.java
+++ src/test/java/dao/SpotifyPlaylistTitleDAOTests.java
@@ -1,2 +1,35 @@
-package dao;public class SpotifyPlaylistTitleDAOTests {
+package dao;
+
+import model.SpotifyPlaylist;
+import model.SpotifyToken;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class SpotifyPlaylistTitleDAOTests {
+
+	private SpotifyPlaylistTitleDAO titleDAO = new SpotifyPlaylistTitleDAO();
+	private SpotifyTokenDAO tokenDAO = new SpotifyTokenDAO();
+	private SpotifyToken token;
+
+	@BeforeEach
+	public void setUp() throws IOException {
+		token = tokenDAO.getToken();
+	}
+
+	@Test
+	public void shouldSayTitleOfPlaylistIsTopFiftyGlobal() throws IOException {
+		String expectedTitle = "Top 50 - Global";
+		String playlistId = "37i9dQZEVXbMDoHDwVN2tF";
+		String tokenString = token.getAccess_token();
+		SpotifyPlaylist emptyPlaylist = new SpotifyPlaylist();
+		SpotifyPlaylist playlist = titleDAO.getPlaylistWithTitle(emptyPlaylist, tokenString, playlistId);
+		String title = playlist.getName();
+
+		assertEquals(expectedTitle, title);
+	}
 }
