7262e6cc28f5065231c6b2db5f2086e3755f6590
th7mo <dev@th7mo.com>
2021-11-21 10:52:10 +0100

    Change that Main now class ApplicationController to control the flow of the program

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,25 +1,12 @@
 package main;
 
-import dao.*;
-import model.SpotifyPlaylist;
-import model.SpotifyToken;
-import model.YoutubePlaylist;
+import controller.ApplicationController;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String playlistId = "0rBbrxmYoRfekosc63tbss";
-        SpotifyTokenDAO spotifyTokenDAO = new SpotifyTokenDAO();
-        SpotifyToken spotifyToken = spotifyTokenDAO.getToken();
-        String tokenString = spotifyToken.getAccess_token();
-        SpotifyPlaylistDAO spotifyPlaylistDAO = new SpotifyPlaylistDAO();
-        SpotifyPlaylist spotifyPlaylist = spotifyPlaylistDAO.getPlaylist(tokenString, playlistId);
-        SpotifyPlaylistTitleDAO titleDAO = new SpotifyPlaylistTitleDAO();
-        SpotifyPlaylist playlistWithTitle = titleDAO.getPlaylistWithTitle(spotifyPlaylist, tokenString, playlistId);
-        YoutubePlaylistDAO youtubePlaylistDAO = new YoutubePlaylistDAO();
-        YoutubePlaylist youtubePlaylist = youtubePlaylistDAO.getPlaylist(playlistWithTitle);
-        youtubePlaylistDAO.postPlaylist(youtubePlaylist, playlistWithTitle);
+        new ApplicationController().convert();
     }
 }
