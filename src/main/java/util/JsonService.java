45108e11cb805f6a82a716dcabc3b9e5b07fcd78
Th7mo <thimoquinten@gmail.com>
2021-11-22 10:35:31 +0100

    Let JsonService use .getClassLoader() to get the json file inside the resource folder

--- src/main/java/util/JsonService.java
+++ src/main/java/util/JsonService.java
@@ -7,18 +7,22 @@ import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 public class JsonService {
 
-    public static String getValueOfKey(String path) {
-        InputStream stream = JsonService.class.getResourceAsStream(path);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+    public static String getValueOfKey(final String key, final String fileName) {
+        InputStream stream = getFileAsInputStream(fileName);
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
         String json = reader.lines().collect(Collectors.joining("\n"));
         JsonElement element = JsonParser.parseString(json);
         JsonObject obj = element.getAsJsonObject();
 
-        return obj.get("key").getAsString();
+        return obj.get(key).getAsString();
+    }
+
+    private static InputStream getFileAsInputStream(final String fileName) {
+        return JsonService.class.getClassLoader().getResourceAsStream(fileName);
     }
 }
